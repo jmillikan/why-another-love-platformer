125ea30d04eb9acb5f4c9665c48947a59c637441
Jesse Millikan <jesse.a.millikan@gmail.com>
2012-07-15 17:13:01 -0400

    Platform moves. (Character does not move with it)

--- platformer/main.lua
+++ platformer/main.lua
@@ -18,7 +18,7 @@ function load_level(level)
 
    platforms = {
       { x = 0, y = 300, width = 100, height = 100 },
-      { x = 140, y = 250, width = 50, height = 20 },
+      { x = 140, y = 250, t = 0, movement = { startx = 140, starty = 250, endx = 260, endy = 250, t = 4 }, width = 50, height = 20,  },
       { x = 300, y = 300, width = 100, height = 100 }
    }
 
@@ -53,13 +53,9 @@ function on_collision(dt, shape_a, shape_b, mtv_x, mtv_y)
       elseif shape_b == playfield.collider then
 
       else
-	 if math.abs(mtv_x) > 0 then
-	    character.x = character.x + mtv_x
-	    character.y = character.y + mtv_y
-	    character.xv = 0
 
-	    character.collider:move(mtv_x, mtv_y)
-	 else
+	 -- TODO: More better
+	 if math.abs(mtv_y) > 0 then
 	    character.x = character.x + mtv_x
 	    character.y = character.y + mtv_y
 	    character.yv = 0
@@ -67,9 +63,20 @@ function on_collision(dt, shape_a, shape_b, mtv_x, mtv_y)
 	    character.collider:move(mtv_x, mtv_y)
 
 	    if character.jumping then
-	       character.yv = -250
+	       character.yv = -400
 	    end
+	 elseif math.abs(mtv_x) > 0 then
+	    character.x = character.x + mtv_x
+	    character.y = character.y + mtv_y
+	    character.xv = 0
+
+	    character.collider:move(mtv_x, mtv_y)
 	 end
+
+	 if math.abs(mtv_x) > 0 and math.abs(mtv_y) > 0 then
+	    print("Diagonal collision")
+	 end
+
       end
    end
 end
@@ -86,7 +93,7 @@ end
 
 
 function love.update(delta)
-   if game_state ~= "running" then return end
+   if game_state ~= "running" and game_state ~= "ending" then return end
 
    if character.running_left then
       character.xv = character.xv - 20
@@ -106,12 +113,48 @@ function love.update(delta)
       character.xv = -100
    end
 
-   character.yv = character.yv + delta * 300
+   character.yv = character.yv + delta * 500
    character.y = character.y + character.yv * delta
    character.x = character.x + character.xv * delta
    character.collider:move(character.xv * delta, character.yv * delta)
 
+   for i,platform in ipairs(platforms) do
+      if platform.movement then
+	 platform.t = platform.t + delta
+
+	 local t = platform.t % platform.movement.t
+
+	 -- for some stupid reason I'm storing the total back-and-forth time...
+	 local segment_t = platform.movement.t / 2
+
+	 local ratio_done
+
+	 if t < segment_t then
+	    startx, starty = platform.movement.startx, platform.movement.starty
+	    endx, endy = platform.movement.endx, platform.movement.endy
+
+	    ratio_done = t / segment_t
+	 else
+	    startx, starty = platform.movement.endx, platform.movement.endy
+	    endx, endy = platform.movement.startx, platform.movement.starty
+
+	    ratio_done = (t - segment_t) / segment_t
+	 end
+
+	 platform.x = (startx * (1 - ratio_done) + endx * ratio_done)
+	 platform.y = (starty * (1 - ratio_done) + endy * ratio_done)
+
+	 update_rect_collider(platform)
+      end
+   end
+
    collider:update(delta)
+end
+
+function update_rect_collider(platform)
+   platform.collider:moveTo(
+      platform.x + (platform.width / 2),
+      platform.y + (platform.height / 2))
 end
 
 function love.keypressed(key, unicode)
@@ -127,6 +170,11 @@ function love.keypressed(key, unicode)
       if key == "up" then
 	 character.jumping = true
       end
+   end
+
+   if key == "n" then
+      reset_character()
+      game_state = "running"
    end
 end
 
