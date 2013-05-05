c701c3e49da9e0de3198e79275aa5f62310b9d74
Jesse Millikan <jesse.a.millikan@gmail.com>
2013-05-05 17:51:01 -0400

    Rebuilt some core pieces around the game rect ADT

--- platformer/main.lua
+++ platformer/main.lua
@@ -2,35 +2,52 @@ HC = require 'hardoncollider'
 _ = require 'underscore.underscore'
 require 'json.json'
 
+--[[
+game rect (convention): A rectangle with top left .x/.y/.width/.height, a collider instance at .collider corresponding to a specific collider object, a graphics at .graphics and an optional angle in radians at .angle
+(move_game_rect, place_game_rect, draw_game_rect)
+
+graphics (for a game rect): One of { color: { r,g,b,a } }, { image: love.graphics image }, {}
+
+TODO: Have a defined graphics aspect of GameRect to simplify drawing
+
+
+--]]
+
 function copy(orig)
    local c = {}
    _.extend(c, orig)
    return c
 end
 
-function collide_rect(rect)
-   rect.collider = collider:addRectangle(rect.x, rect.y, rect.width, rect.height)
-   if rect.angle then
-      rect.collider:rotate(rect.angle)
+
+-- Produce a game rect from a JSON object containing x/y/width/height[/angle], a game rect graphics, and an HC collider
+function load_game_rect(json, collider, graphics)
+   local new_rect = copy(json)
+   new_rect.collider = collider:addRectangle(new_rect.x, new_rect.y, new_rect.width, new_rect.height)
+   if new_rect.angle then
+      new_rect.collider:rotate(new_rect.angle)
    end
+   new_rect.graphics = graphics
+   return new_rect
 end
 
+-- Set global game state to initial state of JSON-loaded level
+-- TODO: Load to a defined game state object
 function load_level(level)
+   -- global
    collider = HC(100, on_collision, collision_stop)
 
-   character = copy(level.character)
-   collide_rect(character)
-   
-   end_door = copy(level.end_door)
-   collide_rect(end_door)
+   character = load_game_rect(level.character, collider, { image = love.graphics.newImage('char.png') })
+   platforms = _.map(level.platforms,
+		     function(p)
+			return load_game_rect(p, collider, { color = {40,230,100,100} })
+		     end)
+   end_door = load_game_rect(level.end_door, collider, { color = {255,255,255,255} })
+   playfield = load_game_rect(level.playfield, collider, { color = {50,50,200,255} })
 
-   platforms = _.map(level.platforms, copy)
-   _.each(platforms, collide_rect)
-   
-   playfield = copy(level.playfield)
-   collide_rect(playfield)
-   playfield.screenx = love.graphics.getWidth() / 2 - playfield.width / 2
-   playfield.screeny = love.graphics.getHeight() / 2 - playfield.height / 2
+   -- "Camera"
+   screenx = love.graphics.getWidth() / 2 - playfield.width / 2
+   screeny = love.graphics.getHeight() / 2 - playfield.height / 2
 end
 
 function love.load()
@@ -49,10 +66,36 @@ end
 
 -- Game mechanics...
 
+-- collider_before_ghosts = {}
+-- collider_after_ghosts = {}
+
+-- Take the x/y "shortest" correction given by HC, 
+-- and convert it into a pure vertical correction
+-- This doesn't give the correct sign.
+function verticalize_correction(mtv_x,mtv_y)
+   if mtv_x ~= 0 then
+      -- print("- x movement (" .. mtv_x .. "): angle collision")
+      return mtv_y - mtv_x / math.atan2(mtv_y, mtv_x)
+   else
+      -- print("- no x movement: non-angle")
+      return mtv_y
+   end
+end
+
+-- Return a with same sign as b
+function match_sign(x, sign)
+   if sign >= 0 then
+      return math.abs(x)
+   else 
+      return -math.abs(x)
+   end
+end
+
 function on_collision(dt, shape_a, shape_b, mtv_x, mtv_y)
    if shape_b == character.collider then
       on_collision(dt, shape_b, shape_a, -mtv_x, -mtv_y)
    elseif shape_a == character.collider then
+
       if shape_b == end_door.collider then
 	 if game_state ~= "ending" then
 	    game_state = "ending"
@@ -64,33 +107,22 @@ function on_collision(dt, shape_a, shape_b, mtv_x, mtv_y)
       elseif shape_b == playfield.collider then
 
       else
-	 -- TODO: More better
-	 if mtv_y ~= 0 then
-	    local dy
-	    if mtv_x ~= 0 then
-	       -- print("Angle collision")
-	       dy = mtv_y - mtv_x / math.atan2(mtv_y, mtv_x)
-	    else
-	       dy = mtv_y
-	    end
-	    
-	    --character.x = character.x + mtv_x
-	    character.y = character.y + dy
-	    character.yv = 0
-	    
-	    character.collider:move(0, dy)
-	    
-	    -- prevents "clining" to platforms form the bottom...
-	    if character.jumping and mtv_y < 0 then
-	       character.yv = -300
-	    end
-	 elseif math.abs(mtv_x) > 0 then
-	    character.x = character.x + mtv_x
-	    character.y = character.y + mtv_y
-	    --	    character.xv = 0
+	 -- table.insert(collider_before_ghosts,copy(character))
 
-	    character.collider:move(mtv_x, mtv_y)
+	 -- TODO: More better
+	 local dy = verticalize_correction(mtv_x, mtv_y)
+	 dy = match_sign(dy, mtv_y)
+	 
+	 move_game_rect(character, 0, dy)
+	 
+	 -- If attempting to jump on a downward collision (INCORRECT!) give jump velocity.
+	 -- TODO: foot collider
+	 if character.jumping and mtv_y < 0 then
+	    character.yv = -300
+	 else
+	    character.yv = 0
 	 end
+	 -- table.insert(collider_after_ghosts,copy(character))
       end
    end
 end
@@ -107,12 +139,15 @@ end
 
 x = 0   
 
+-- TODO: Either normalize delta of game updates or (less likely) fix mechanics to correct for size of delta
 function love.update(delta)
    x = x + 1
    
    if game_state ~= "running" and game_state ~= "ending" then return end
 
    if game_state == "running" or game_state == "ending" then
+
+      -- Use running bits to modify x velocity
       if character.running_left then
 	 character.xv = character.xv - 20
       elseif character.running_right then
@@ -125,19 +160,22 @@ function love.update(delta)
 	 character.xv = 0
       end
 
+      -- cap x velocity
       if character.xv > 100 then
 	 character.xv = 100
       elseif character.xv < -100 then
 	 character.xv = -100
       end
 
-
+      -- Use constants and delta to modify y velocity
       character.yv = character.yv + delta * 500
-      character.y = character.y + character.yv * delta
-      character.x = character.x + character.xv * delta
-      character.collider:move(character.xv * delta, character.yv * delta)
 
+      -- Move character by x and y
+      move_game_rect(character, character.xv * delta, character.yv * delta)
+
+      -- For each platform...
       for platform in _.iter(platforms) do
+	 -- Move back and forth on a set schedule...
 	 if platform.movement then
 	    platform.t = platform.t + delta
 
@@ -160,17 +198,18 @@ function love.update(delta)
 	       ratio_done = (t - segment_t) / segment_t
 	    end
 
-	    platform.x = (startx * (1 - ratio_done) + endx * ratio_done)
-	    platform.y = (starty * (1 - ratio_done) + endy * ratio_done)
-
-	    update_rect_collider(platform)
+	    place_game_rect(platform, 
+			    (startx * (1 - ratio_done) + endx * ratio_done),
+			    (starty * (1 - ratio_done) + endy * ratio_done))
 	 end
       end
 
       -- TODO: Collect collision events and resolve in a manner consistent with character movement to prevent platform penetration...
+
+      -- (COLLISION EVENTS OCCUR)
+      -- Try separating x and y movements and collisions!
+
       collider:update(delta)
-
-
    end
 
    if game_state == "ending" then
@@ -185,12 +224,6 @@ function love.update(delta)
 	 end
       end
    end
-end
-
-function update_rect_collider(platform)
-   platform.collider:moveTo(
-      platform.x + (platform.width / 2),
-      platform.y + (platform.height / 2))
 end
 
 function love.keypressed(key, unicode)
@@ -241,8 +274,17 @@ end
 -- graphics
 function love.draw()
    if game_state == "running" or game_state == "ending" then
-      draw_level()
-      draw_character()
+
+      love.graphics.push()
+
+      love.graphics.translate(screenx, screeny)
+      draw_game_rect(playfield)
+      _.each(platforms, draw_game_rect)
+      draw_game_rect(end_door)
+      draw_game_rect(character)
+
+      love.graphics.pop()
+
       if game_state == "ending" then
 	 show_text("Moving on...", 200)
       end
@@ -259,35 +301,43 @@ function show_text(text, height)
    love.graphics.printf(text, 0, 100 + height, love.graphics.getWidth(), "center")
 end
 
-function draw_character()
-   love.graphics.push()
-   love.graphics.translate(playfield.screenx + character.x + character.width / 2, playfield.screeny + character.y + character.height / 2)
+-- Places a game rect *by its center* at the given x and y
+function place_game_rect(r, x, y)
+   r.x = x
+   r.y = y
 
-   --   love.graphics.rotate(math.pi / 4)
+   r.collider:moveTo(
+      r.x + (r.width / 2),
+      r.y + (r.height / 2))
 
-   love.graphics.draw(character_graphic, - character.width / 2, - character.height / 2)
-   love.graphics.pop()
+   -- TODO: Figure out why this works with angled platforms...
 end
 
-function draw_level()
-   love.graphics.setColor(50, 50, 200)
-   draw_game_rect(playfield)
 
-   love.graphics.setColor(40, 230, 100, 100)
-   _.each(platforms, draw_game_rect)
+function move_game_rect(r, dx, dy)
+   r.x = r.x + dx
+   r.y = r.y + dy
 
-   love.graphics.setColor(230,230,230)
-   draw_game_rect(end_door)
+   r.collider:move(dx, dy)
 end
 
 function draw_game_rect(r)
    love.graphics.push()
-   love.graphics.translate(playfield.screenx + r.x + r.width / 2, playfield.screeny + r.y + r.height / 2)
+
+   love.graphics.translate(r.x + r.width / 2, r.y + r.height / 2)
 
    if r.angle then
       love.graphics.rotate(r.angle)
    end
 
-   love.graphics.rectangle("fill", - r.width / 2,  - r.height / 2, r.width, r.height)
+   -- See "Graphics"
+   local g = r.graphics
+   if g.color then
+      love.graphics.setColor(unpack(g.color))
+      love.graphics.rectangle("fill", - r.width / 2,  - r.height / 2, r.width, r.height)
+   elseif g.image then
+      love.graphics.draw(g.image, - r.width / 2, - r.height / 2)
+   end -- {} - do nothing
+
    love.graphics.pop()
 end
