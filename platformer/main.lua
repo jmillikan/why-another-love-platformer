01652dfa3fc07f648a674e2686c3e5289ab4ed15
Jesse Millikan <jesse.a.millikan@gmail.com>
2013-05-05 19:26:48 -0400

    Global state relegated to love.* through level state type

--- platformer/main.lua
+++ platformer/main.lua
@@ -4,73 +4,164 @@ require 'json.json'
 
 --[[
 Globals: 
-level_state - The current level state
-game_state - "front", "running", "ending", "won"
-levels - Array of loaded level tables
+_level_state - The current level state - character position, platform positions, dead/finished
+_game_state - "front", "running", "ending", "won"
+_end_time - A countdown from "ending" to next state while showing level
+_levels - Array of loaded level tables
 --]]
 
 --[[
-game rect: A possibly angled game rectangle that can be drawn, moved, that causes collision detection
+game rect (convention): A possibly angled game rectangle that can be drawn, can be moved, and causes collision handling
 Functions: load_game_rect, move_game_rect, place_game_rect, draw_game_rect
-(Most game rects have some non-abstract fields handled directly.)
 
 game rect graphics: One of { color: { r,g,b,a } }, { image: love.graphics image }, {}
 
 level state:
-A table of
-.collider - HC instance responsible for game rect colliders
-.character - character game rect
-.platforms - array of platform game rects
-.end_door - end door game rect
-.playfield - playfield game rect
-.screenx/.screeny - screen x/y where game objects should be drawn
+   make_level_state, advance_level_state, draw_level_state
+   .dead, .finished
+   .character.running_left... (TODO: abstract these into keypressed_level_state etc.)
 --]]
 
-function copy(orig)
-   local c = {}
-   _.extend(c, orig)
-   return c
+function love.load()
+   local contents, length = love.filesystem.read("levels.json")
+
+   _levels = json.decode(contents)
+
+   _game_state = "front"
 end
 
--- JSON rect: A table with x/y/width/height[/angle]
+-- TODO: Either normalize delta of game updates or (less likely) fix mechanics to correct for size of delta
+function love.update(delta)
+   if _game_state ~= "running" and _game_state ~= "ending" then return end
 
+   advance_level_state(_level_state, delta)
+
+   if _level_state.dead then
+      -- Dead, restart current level
+      _level_state = make_level_state(_levels[current_level])
+   elseif _level_state.finished then
+      if _game_state ~= "ending" then
+	 _game_state = "ending"
+	 _ending_time = 2
+      end
+   end
+
+   -- game state update may end game
+   if _game_state == "ending" then
+      _ending_time = _ending_time - delta
+      if _ending_time < 0 then
+	 current_level = current_level + 1
+	 if current_level > #_levels then
+	    _game_state = "won"
+	 else
+	    _level_state = make_level_state(_levels[current_level])
+	    _game_state = "running"
+	 end
+      end
+   end
+end
+
+function love.keypressed(key, unicode)
+   -- Note: After _level_state is modified, character will be old
+
+   if _game_state == "running" then
+      local character = _level_state.character
+
+      if key == "r" then
+	 _level_state = make_level_state(_levels[current_level])
+	 _game_state = "running"
+      else
+	 keypress_level_state(_level_state, key)
+      end
+   end
+
+   if _game_state == "front" or _game_state == "won" then
+      if key == "n" then
+	 current_level = 1
+	 _level_state = make_level_state(_levels[current_level])
+	 _game_state = "running"
+      end
+   end
+end
+
+function love.keyreleased(key, unicode)
+   if _game_state == "running" then
+      -- Note: After _level_state is modified, character will be old
+      local character = _level_state.character
+      
+      if _game_state == "running" then
+	 keyrelease_level_state(_level_state, key)
+      end
+   end
+end
+
+function love.draw()
+   if _game_state == "running" or _game_state == "ending" then
+      
+      draw_level_state(_level_state)
+
+      if _game_state == "ending" then
+	 show_text("Moving on...", 200)
+      end
+   elseif _game_state == "won" then
+      show_text("You won! Another game?", 0)
+      show_text("Press n to start", 100)
+   elseif _game_state == "front" then
+      show_text("Why Another Love Platformer", 0)
+      show_text("Press n to start", 100)
+   end
+end
+
+-- JSON rect: A table with .x/.y/.width/.height[/.angle]
 -- Produce a level state to initial state of JSON-loaded level
 function make_level_state(level)
    local ls = {}
-   -- global
-   local collider = HC(100, on_collision, collision_stop)
+   local collider = HC(100, 
+		       function(...) start_collision_level_state(ls, ...) end,
+		       function(...) stop_collision_level_state(ls, ...) end)
+
    local playfield = load_game_rect(level.playfield, collider, { color = {50,50,200,255} })
    
-   return { 
-      collider = collider,
-      character = load_game_rect(level.character, collider, { image = love.graphics.newImage('char.png') }),
-      end_door = load_game_rect(level.end_door, collider, { color = {255,255,255,255} }),
-      platforms = _.map(level.platforms,
-			function(p)
-			   return load_game_rect(p, collider, { color = {40,230,100,100} })
-			end),
-      playfield = playfield,
-      screenx = love.graphics.getWidth() / 2 - playfield.width / 2,
-      screeny = love.graphics.getHeight() / 2 - playfield.height / 2
-	  }
+   return _.extend(ls, 
+		   { 
+		      dead = false,
+		      collider = collider,
+		      character = load_game_rect(level.character, collider, { image = love.graphics.newImage('char.png') }),
+		      end_door = load_game_rect(level.end_door, collider, { color = {255,255,255,255} }),
+		      platforms = _.map(level.platforms,
+					function(p)
+					   return load_game_rect(p, collider, { color = {40,230,100,100} })
+					end),
+		      playfield = playfield,
+		      screenx = love.graphics.getWidth() / 2 - playfield.width / 2,
+		      screeny = love.graphics.getHeight() / 2 - playfield.height / 2
+		   })
 end
 
-function love.load()
-   levels = parse_levels()
-
-   game_state = "front"
+-- key: A love2d KeyConstant
+function keypress_level_state(ls, key)
+   if key == "right" then
+      ls.character.running_right = true
+   elseif key == "left" then
+      ls.character.running_left = true
+   elseif key == "up" then
+      ls.character.jumping = true
+   end
 end
 
-function parse_levels()
-   local contents, length = love.filesystem.read("levels.json")
-
-   return json.decode(contents)
+-- key: A love2d KeyConstant
+function keyrelease_level_state(ls, key)
+   if key == "right" then
+      ls.character.running_right = false
+   elseif key == "left" then
+      ls.character.running_left = false
+   elseif key == "up" then
+      ls.character.jumping = false
+   end
 end
 
--- Game mechanics...
-
--- collider_before_ghosts = {}
--- collider_after_ghosts = {}
+-- Handle collisions wrt a level state
+-- TODO: get _game_state and _level_state out, handle those by a new _game_state or something
 
 -- Take the x/y "shortest" correction given by HC, 
 -- and convert it into a pure vertical correction
@@ -87,39 +178,28 @@ end
 
 -- Return a with same sign as b
 function match_sign(x, sign)
-   if sign >= 0 then
-      return math.abs(x)
-   else 
-      return -math.abs(x)
-   end
+   return sign >= 0 and math.abs(x) or -math.abs(x)
 end
 
--- TODO: wrap on_collision somehow to get level_state interior
-function on_collision(dt, shape_a, shape_b, mtv_x, mtv_y)
-   local character = level_state.character
-   local end_door = level_state.end_door
-   local playfield = level_state.playfield
+function start_collision_level_state(ls, dt, shape_a, shape_b, mtv_x, mtv_y)
+   local character = ls.character
+   local end_door = ls.end_door
+   local playfield = ls.playfield
 
    if shape_b == character.collider then
-      on_collision(dt, shape_b, shape_a, -mtv_x, -mtv_y)
+      start_collision_level_state(ls, dt, shape_b, shape_a, -mtv_x, -mtv_y)
    elseif shape_a == character.collider then
 
       if shape_b == end_door.collider then
-	 if game_state ~= "ending" then
-	    game_state = "ending"
-	    ending_time = 2
-	    character.running_left = false
-	    character.running_right = false
-	    character.jumping = false
-	 end
+	 -- May happen multiple times
+	 ls.finished = true
+	 character.running_left = false
+	 character.running_right = false
+	 character.jumping = false
       elseif shape_b == playfield.collider then
 
       else
-	 -- table.insert(collider_before_ghosts,copy(character))
-
-	 -- TODO: More better
-	 local dy = verticalize_correction(mtv_x, mtv_y)
-	 dy = match_sign(dy, mtv_y)
+	 local dy = match_sign(verticalize_correction(mtv_x, mtv_y), mtv_y)
 	 
 	 move_game_rect(character, 0, dy)
 	 
@@ -130,34 +210,28 @@ function on_collision(dt, shape_a, shape_b, mtv_x, mtv_y)
 	 else
 	    character.yv = 0
 	 end
-	 -- table.insert(collider_after_ghosts,copy(character))
       end
    end
 end
 
-function collision_stop(dt, shape_a, shape_b)
-   local character = level_state.character
-   local playfield = level_state.playfield
+function stop_collision_level_state(ls, dt, shape_a, shape_b)
+   local character = ls.character
+   local playfield = ls.playfield
 
    if shape_b == character.collider then
-      collision_stop(dt, shape_b, shape_a)
+      stop_collision_level_state(ls, dt, shape_b, shape_a)
    elseif shape_a == character.collider then
       
       -- "Die" if out of the playfield, handles falling...
       if shape_b == playfield.collider then 
-	 level_state = make_level_state(levels[current_level])
+	 ls.dead = true
       end
    end
 end
 
-x = 0   
-
--- TODO: Either normalize delta of game updates or (less likely) fix mechanics to correct for size of delta
-function love.update(delta)
-   if game_state ~= "running" and game_state ~= "ending" then return end
-
-   local character = level_state.character
-   local platforms = level_state.platforms
+function advance_level_state(ls, delta)
+   local character = ls.character
+   local platforms = ls.platforms
    
    -- Use running bits to modify x velocity
    if character.running_left then
@@ -202,11 +276,11 @@ function love.update(delta)
 	    endx, endy = platform.movement.endx, platform.movement.endy
 	    
 	    ratio_done = t / segment_t
-	    else
-	       startx, starty = platform.movement.endx, platform.movement.endy
-	       endx, endy = platform.movement.startx, platform.movement.starty
-	       
-	       ratio_done = (t - segment_t) / segment_t
+	 else
+	    startx, starty = platform.movement.endx, platform.movement.endy
+	    endx, endy = platform.movement.startx, platform.movement.starty
+	    
+	    ratio_done = (t - segment_t) / segment_t
 	 end
 	 
 	 place_game_rect(platform, 
@@ -215,106 +289,21 @@ function love.update(delta)
       end
    end
 
-      -- TODO: Collect collision events and resolve in a manner consistent with character movement to prevent platform penetration...
+   -- TODO: Collect collision events and resolve in a manner consistent with character movement to prevent platform penetration...
+   ls.collider:update(delta)
+end
 
-      -- (COLLISION EVENTS OCCUR)
-      -- Try separating x and y movements and collisions!
-
-   level_state.collider:update(delta)
+function draw_level_state(ls)
+   love.graphics.push()
    
-   if game_state == "ending" then
-      ending_time = ending_time - delta
-      if ending_time < 0 then
-	 current_level = current_level + 1
-	 if current_level > #levels then
-	    game_state = "won"
-	 else
-	    level_state = make_level_state(levels[current_level])
-	    game_state = "running"
-	 end
-      end
-   end
-end
-
-function love.keypressed(key, unicode)
-   -- Note: After level_state is modified, character will be old
-
-   if game_state == "running" then
-      local character = level_state.character
-
-      if key == "right" then
-	 character.running_right = true
-      end
-
-      if key == "left" then
-	 character.running_left = true
-      end
-
-      if key == "up" then
-	 character.jumping = true
-      end
-
-      if key == "r" then
-	 level_state = make_level_state(levels[current_level])
-	 game_state = "running"
-      end
-   end
-
-   if game_state == "front" or game_state == "won" then
-      if key == "n" then
-	 current_level = 1
-	 level_state = make_level_state(levels[current_level])
-	 game_state = "running"
-      end
-   end
-end
-
-function love.keyreleased(key, unicode)
-   if game_state == "running" then
-      -- Note: After level_state is modified, character will be old
-      local character = level_state.character
-      
-      if game_state == "running" then
-	 if key == "right" then
-	    character.running_right = false
-	 end
-	 
-	 if key == "left" then
-	 character.running_left = false
-	 end
-	 
-	 if key == "up" then
-	    character.jumping = false
-	 end
-      end
-   end
-end
-
--- graphics
-function love.draw()
-   if game_state == "running" or game_state == "ending" then
-
-      love.graphics.push()
-
-      love.graphics.translate(level_state.screenx, level_state.screeny)
-      draw_game_rect(level_state.playfield)
-      _.each(level_state.platforms, draw_game_rect)
-      draw_game_rect(level_state.end_door)
-      draw_game_rect(level_state.character)
-
-      love.graphics.pop()
-
-      if game_state == "ending" then
-	 show_text("Moving on...", 200)
-      end
-   elseif game_state == "won" then
-      show_text("You won! Another game?", 0)
-      show_text("Press n to start", 100)
-   elseif game_state == "front" then
-      show_text("Why Another Love Platformer", 0)
-      show_text("Press n to start", 100)
-   end
-end
+   love.graphics.translate(ls.screenx, ls.screeny)
+   draw_game_rect(ls.playfield)
+   _.each(ls.platforms, draw_game_rect)
+   draw_game_rect(ls.end_door)
+   draw_game_rect(ls.character)
+   
+   love.graphics.pop()
+end   
 
 function show_text(text, height)
    love.graphics.printf(text, 0, 100 + height, love.graphics.getWidth(), "center")
@@ -326,7 +315,8 @@ Game Rect internals: A rectangle with top left .x/.y/.width/.height, a collider 
 
 -- Produce a game rect from JSON rect, an HC collider and a game rect raphics
 function load_game_rect(json, collider, graphics)
-   local new_rect = copy(json)
+   local new_rect = {}
+   _.extend(new_rect, json)
    new_rect.collider = collider:addRectangle(new_rect.x, new_rect.y, new_rect.width, new_rect.height)
    if new_rect.angle then
       new_rect.collider:rotate(new_rect.angle)
@@ -334,7 +324,6 @@ function load_game_rect(json, collider, graphics)
    new_rect.graphics = graphics
    return new_rect
 end
-
 
 -- Places a game rect *by its center* at the given x and y
 function place_game_rect(r, x, y)
@@ -347,7 +336,6 @@ function place_game_rect(r, x, y)
 
    -- TODO: Figure out why this works with angled platforms...
 end
-
 
 function move_game_rect(r, dx, dy)
    r.x = r.x + dx
